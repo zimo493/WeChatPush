5f92369a8edd8023b9fec6ee55817f8553d07b0c
子陌 <88411811+zimo493@users.noreply.github.com>
2022-10-12 14:08:39 +0800

    添加星座配置项

--- noon/config.js
+++ noon/config.js
@@ -6,6 +6,8 @@ const config = {
   template_id: "", // 发送模板 id
   // 信息配置
   user_name: '张三', // 她或他的名字
+  /* aries 白羊座, taurus 金牛座, gemini 双子座, cancer 巨蟹座 leo 狮子座, virgo 处女座 libra 天秤座, scorpio 天蝎座 sagittarius 射手座, capricorn 摩羯座, aquarius 水瓶座, pisces 双鱼座 */
+  constellation: '',
   city: "合肥", // 所在城市(可精确到市/区)
   tian_api: "", // 天行数据apikey
 }
