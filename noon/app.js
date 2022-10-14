9f464e59d96d5b0f66c0dbfa7adb9ed7fc996c9a
子陌 <88411811+zimo493@users.noreply.github.com>
2022-10-14 15:44:36 +0800

    优化代码

--- noon/app.js
+++ noon/app.js
@@ -1,4 +1,4 @@
-const config = require("./config")
+const { app_id, app_secret, city, constellation, template_id, tian_api, user: { forEach }, user_name } = require("./config")
 const axios = require('axios');
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
@@ -23,60 +23,40 @@ const axiosPost = (url, params) => {
 
 // 获取token
 const getToken = async () => {
-  const params = {
+  let { data: { access_token } } = await axiosGet('https://api.weixin.qq.com/cgi-bin/token', {
     grant_type: 'client_credential',
-    appid: config.app_id,
-    secret: config.app_secret,
-  };
-  let res = await axiosGet('https://api.weixin.qq.com/cgi-bin/token', params);
-  return res.data.access_token;
+    appid: app_id,
+    secret: app_secret,
+  });
+  return access_token;
 }
 
 // 获取今日运势
 const get_dayluck_data = async () => {
-  let params = {
-    key: config.tian_api,
-    astro: config.constellation
-  }
-  let res = await axiosGet("http://api.tianapi.com/star/index", params);
-  if (res.data.code !== 200) return console.log('获取今日运势失败！！');
-  console.log(res.data.newslist);
-  return res.data.newslist[res.data.newslist.length - 1].content
+  let { data: { code, newslist } } = await axiosGet("http://api.tianapi.com/star/index", {
+    key: tian_api,
+    astro: constellation
+  });
+  console.log(newslist);
+  return (code !== 200) ? console.log('获取今日运势失败！！') : newslist[newslist.length - 1].content;
 }
 
 // 随机颜色
-const randomColor = () => {
-  return "#" + parseInt(Math.random() * 0x1000000).toString(16).padStart(6, "0")
-}
+const randomColor = () => '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 
 // 疫情数据
 const yq = async () => {
-  let params = {
-    city: config.city,
-  }
-  let res = await axiosGet("https://covid.myquark.cn/quark/covid/data", params)
-  let data = void 0;
-  if (["北京", "上海", "天津", "重庆", "香港", "澳门", "台湾"].includes(config.city)) data = res.data.provinceData
-  else data = res.data.cityData
-  let sure_new_loc = `新增本土：${data.sure_new_loc}`;
-  let sure_new_hid = `新增无症状：${data.sure_new_hid}`;
-  let sure_cnt = `累计确诊：${data.sure_cnt}`;
-  let cure_cnt = `累计治愈：${data.cure_cnt}`;
-  let present = `现有确诊：${data.present === '-' ? 0 : data.present}`;
-  let danger = `中/高风险区：${data.danger["1"]} / ${data.danger["2"]}`;
-  let statistics_time = res.data.time
-  return `${config.city}市本地疫情数据\n${sure_new_loc}，${sure_new_hid}\n${sure_cnt}，${cure_cnt}\n${present}\n${danger}\n${statistics_time}`
+  let { data: { cityData, provinceData, time } } = await axiosGet("https://covid.myquark.cn/quark/covid/data", { city: city })
+  let { cure_cnt, danger, present, sure_cnt, sure_new_hid, sure_new_loc } = ([
+    "北京", "上海", "天津", "重庆", "香港", "澳门", "台湾"
+  ].includes(city)) ? provinceData : cityData
+  return `${city}本地疫情数据\n新增本土: ${sure_new_loc}\n新增无症状: ${sure_new_hid}\n累计确诊: ${sure_cnt}，累计治愈: ${cure_cnt}\n现有确诊: ${present === '-' ? 0 : present}\n中/高风险区：${danger["1"]}/${danger["2"]}\n${time}`
 }
 
 // 彩虹屁
 const get_caihongpi_data = async () => {
-  let params = {
-    key: config.tian_api,
-  }
-  let res = await axiosGet("http://api.tianapi.com/caihongpi/index", params);
-  if (res.data.code !== 200) return console.log('获取彩虹屁数据失败！！');
-  console.log('彩虹屁：', res.data.newslist[0].content);
-  return res.data.newslist[0].content;
+  let { data: { code, newslist } } = await axiosGet("http://api.tianapi.com/caihongpi/index", { key: tian_api, });
+  return code !== 200 ? console.log('获取彩虹屁数据失败！！') : newslist[0].content;
 }
 
 const templateMessageSend = async () => {
@@ -90,54 +70,33 @@ const templateMessageSend = async () => {
   let caihongpi_data = await get_caihongpi_data()
   // 模板id 配置项
   const params = {
-    // touser: config.user,
-    template_id: config.template_id,
+    template_id: template_id,
     url: 'http://weixin.qq.com/download',
     topcolor: '#FF0000',
     data: {
       // 宝贝的名字
-      dearName: {
-        value: config.user_name,
-        color: randomColor(),
-      },
+      dearName: { value: user_name, color: randomColor(), },
       // 今日运势
-      dayluck: {
-        value: dayluck_data,
-        color: randomColor(),
-      },
+      dayluck: { value: dayluck_data, color: randomColor(), },
       // 疫情数据
-      yq_data: {
-        value: yqData,
-        color: '#000'
-      },
+      yq_data: { value: yqData, color: '#000' },
       // 彩虹屁
-      caihongpi: {
-        value: caihongpi_data,
-        color: randomColor(),
-      }
+      caihongpi: { value: caihongpi_data, color: randomColor(), }
     },
   };
-  config.user.forEach(async item => {
+  forEach(async item => {
     params.touser = item;
-    let res = await axiosPost(url, params);
-    switch (res.data.errcode) {
-      case 40001:
-        console.log("推送消息失败,请检查 appId/appSecret 是否正确");
-        break
-      case 40003:
-        console.log("推送消息失败,请检查微信号是否正确");
-        break
-      case 40037:
-        console.log("推送消息失败,请检查模板id是否正确");
-        break
-      case 0:
-        console.log("推送消息成功");
-        break
-    }
+    let { data: { errcode } } = await axiosPost(url, params);
+    console.log({
+      40001: "推送消息失败! 请检查 appId/appSecret 是否正确",
+      40003: "推送消息失败! 请检查微信号是否正确",
+      40037: "推送消息失败! 请检查模板id是否正确",
+      0: `${item}: 推送消息成功`,
+    }[errcode]);
   })
 }
 // 调用函数，推送模板消息
-templateMessageSend(); // 第一次执行程序时会推送一次消息
+// templateMessageSend(); // 第一次执行程序时会推送一次消息
 
 // 定时器（Cron）：定时推送消息
 const schedule = require('node-schedule');
